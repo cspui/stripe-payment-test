1c62bcfb5cf02df24cf9b4ad758b112682ee361c
CheeSheng <cspui14@yahoo.com.my>
2021-11-28 15:56:43 +0800

    Added FPX payment and apply stripe styling

--- server/public/payment.js
+++ server/public/payment.js
@@ -1,21 +1,88 @@
-const button = document.querySelector("button")
 
-button.addEventListener("click", () => {
-    fetch('/checkout-session', {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            items: [
-                {id: 1, quantity: 3},
-                {id: 2, quantity: 2}
-            ],
-        }),
-    }).then(res => {
-        if (res.ok) return res.json();
-        return res.json().then(json => Promise.reject(json));
-    }).then(({ url }) => {
-        window.location = url;
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+    // Normal payment (USD)
+    const normal_btn = document.querySelector("#normal")
+
+    normal_btn.addEventListener("click", () => {
+        // Make a call to the server for payment intent 
+        fetch('/checkout-session', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                items: [
+                    { id: 1, quantity: 3 },
+                    { id: 2, quantity: 2 }
+                ],
+            }),
+        }).then(res => {
+            if (res.ok) return res.json();
+            return res.json().then(json => Promise.reject(json));
+        }).then(({ url }) => {
+            window.location = url;
+        })
     })
-})
+
+
+    // FPX payment (RM)
+    const {publishableKey} = await fetch('/config').then(r => r.json());
+    const stripe = Stripe(publishableKey);
+
+    const elements = stripe.elements();
+    const fpxBank = elements.create('fpxBank', {
+        accountHolderType: 'individual',
+    });
+    fpxBank.mount('#fpx-bank-element');
+
+    // When the form is submitted...
+    var form = document.getElementById('payment-form');
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        // Make a call to the server to create a new
+        // payment intent and store its client_secret.
+        const { error: backendError, clientSecret } = await fetch(
+            '/checkout-session-fpx',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    currency: 'myr',
+                    paymentMethodType: 'fpx',
+                }),
+            }
+        ).then((r) => r.json());
+
+        if (backendError) {
+            addMessage(backendError.message);
+            return;
+        }
+
+        addMessage(`Client secret returned.`);
+
+        // Confirm the fpxBank payment given the clientSecret
+        // from the payment intent that was just created on
+        // the server.
+        const { error: stripeError, paymentIntent } = await stripe.confirmFpxPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    fpx: fpxBank,
+                },
+                return_url: `${window.location.origin}/return.html`,
+            }
+        );
+
+        if (stripeError) {
+            addMessage(stripeError.message);
+        }
+
+        addMessage(`Payment ${paymentIntent.status}: ${paymentIntent.id}`);
+    });
+
+
+});
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 /*
-    Run server: npm run devStart (with nodemon)
+    Run server: npm run devStart (with nodemon in server dir)
                 node server.js
 */
 
@@ -13,9 +13,16 @@ app.use(express.static("public"));
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 const storeItems = new Map([
-  [1, { priceInCents: 10000, name: "Item 1" }],
-  [2, { priceInCents: 20000, name: "Item 2" }],
+    [1, { priceInCents: 10000, name: "Item 1" }],
+    [2, { priceInCents: 20000, name: "Item 2" }],
 ]);
+
+
+app.get('/config', (req, res) => {
+    res.send({
+        publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    });
+});
 
 app.post('/checkout-session', async (req, res) => {
     try {
@@ -38,10 +45,59 @@ app.post('/checkout-session', async (req, res) => {
             success_url: `${process.env.SERVER_URL}/success.html`,
             cancel_url: `${process.env.SERVER_URL}/cancel.html`
         })
-        res.json({ url: session.url})
+        res.json({ url: session.url })
     } catch (e) {
         res.status(500).json({ error: e.message })
     }
 })
+
+app.post('/checkout-session-fpx', async (req, res) => {
+    const { paymentMethodType, currency } = req.body;
+
+    // Each payment method type has support for different currencies. In order to
+    // support many payment method types and several currencies, this server
+    // endpoint accepts both the payment method type and the currency as
+    // parameters.
+    //
+    // Some example payment method types include `card`, `ideal`, and `alipay`.
+    const params = {
+        payment_method_types: [paymentMethodType],
+        amount: 1999,
+        currency: currency,
+    }
+
+    // If this is for an ACSS payment, we add payment_method_options to create
+    // the Mandate.
+    if (paymentMethodType === 'acss_debit') {
+        params.payment_method_options = {
+            acss_debit: {
+                mandate_options: {
+                    payment_schedule: 'sporadic',
+                    transaction_type: 'personal',
+                },
+            },
+        }
+    }
+
+    // Create a PaymentIntent with the amount, currency, and a payment method type.
+    //
+    // See the documentation [0] for the full list of supported parameters.
+    //
+    // [0] https://stripe.com/docs/api/payment_intents/create
+    try {
+        const paymentIntent = await stripe.paymentIntents.create(params);
+
+        // Send publishable key and PaymentIntent details to client
+        res.send({
+            clientSecret: paymentIntent.client_secret,
+        });
+    } catch (e) {
+        return res.status(400).send({
+            error: {
+                message: e.message,
+            },
+        });
+    }
+});
 
 app.listen(3000);
