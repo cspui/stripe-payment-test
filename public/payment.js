b52998d3e6e12c90464f22ce6fe1acec4758bec9
CheeSheng <cspui14@yahoo.com.my>
2021-12-19 00:21:32 +0800

    added paypal payment

--- public/payment.js
+++ public/payment.js
@@ -1,8 +1,8 @@
-
-
 document.addEventListener('DOMContentLoaded', async () => {
 
-    // Normal payment (USD)
+
+    //============stripe===============
+    // Normal banking payment (USD)
     const normal_btn = document.querySelector("#normal")
 
     normal_btn.addEventListener("click", () => {
@@ -84,5 +84,35 @@ document.addEventListener('DOMContentLoaded', async () => {
         addMessage(`Payment ${paymentIntent.status}: ${paymentIntent.id}`);
     });
 
+
+
+
+    //===========paypal=============
+    let amountElement = document.getElementById("amount")
+    paypal.Buttons({
+        // Sets up the transaction when a payment button is clicked
+        createOrder: function(data, actions) {
+          return actions.order.create({
+            purchase_units: [{
+              amount: {
+                value: amountElement.value 
+              }
+            }]
+          });
+        },
+
+        // Finalize the transaction after payer approval
+        onApprove: function(data, actions) {
+          return actions.order.capture().then(function(orderData) {
+            // Successful capture! For dev/demo purposes:
+                console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                var transaction = orderData.purchase_units[0].payments.captures[0];
+                alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+
+            // When ready to go live, remove the alert and show a success message within this page. For example:
+            // Or go to another URL:  actions.redirect('thank_you.html');
+          });
+        }
+      }).render('#paypal');
 
 });
